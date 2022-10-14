9a6ce39945fe6ab4004d5e3794ee56b41c2d74ee
matthew9252 <matthew9252@naver.com>
2022-10-14 19:49:40 +0900

    사용자로부터 입력 인자 저장하기

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+
 import telegram
 from telegram.ext import Updater
-from telegram.ext import MessageHandler, Filters
+from telegram.ext import MessageHandler, Filters, CommandHandler
 
 my_token = "5336689796:AAGL3VEA1xM9dDodIbDxDNvv6Fv-VirCQoA"
 bot = telegram.Bot(token=my_token)
@@ -11,12 +12,17 @@ updater = Updater(token=my_token, use_context=True)
 dispatcher = updater.dispatcher
 updater.start_polling()
 
-def echo(update, context):
-    #  user_text = update.message.text 
-    #  if user_text == "안녕":
-    #     bot.send_message(chat_id=id, text="안녕안녕")
 
-    bot.send_message(chat_id=id, text="어쩔 ㅋ")
+def echo(update, context):
+    user_text = update.message.text 
+    if user_text == "안녕":
+        bot.sendMessage(chat_id=id, text="안녕안녕")
+def ncal(update, context):
+    input = context.args[0]
+    bot.sendMessage(chat_id=id, text=input)
+
+
 
 echo_handler = MessageHandler(Filters.text, echo)
+updater.dispatcher.add_handler(CommandHandler('ncal', ncal))
 dispatcher.add_handler(echo_handler)
