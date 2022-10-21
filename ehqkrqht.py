78a2452e7ba7d66f8c770e0f3f5f88e6ce80afdc
문지성 <jsmoon4168@naver.com>
2022-10-21 23:59:35 +0900

    first commit

--- ehqkrqht.py
+++ ehqkrqht.py
@@ -1,38 +1,8 @@
-import random
-import os
-import time
+cost = int(input("금액을 입력하시오: "))
+people = int(input("몇명인지입력하시오: "))
 
+average = (cost / people)
+print("1인당 %d원씩 지불하시면 됩니다"% average)
 
-life = 5
-score = 0
-
-while 1:
-    if life == 0:
-        print("게임 종료")
-        break
-
-    N = random.randint(1,99)
-    print("점수 : {}, 목숨 : {}".format(score, life))
-    user = int(input("홀(0), 짝(1) : "))
-
-    print("정답 : {}".format(N), end='/ ')
-
-    if user:
-        if N % 2:
-            print("틀렸습니다.")
-            life -= 1
-        else:
-            print("맞았습니다.")
-            score += 100
-    else:
-        if N % 2:
-            print("맞았습니다.")
-            score += 100
-        else:
-            print("틀렸습니다.")
-            life -= 1
-
-    time.sleep(2)               # 프로그램을 잠시 멈춤
-    os.system("cls")            # 화면을 바꿔줌
-
-print("총 점수 : ", score)
+if (cost % people) != 0:
+    print("남은 금액은 가위바위보를 통해 당첨자가 %d원을 지불하시면 됩니다^^"%(cost % people))
---
+++ wpqlQhqrl.py
@@ -0,0 +1,29 @@
+import random
+import re
+
+#숫자인지 문자인지 구별
+while True:
+    range = input("몇명인가요? ")
+
+    if re.match("/d",range) and not re.match('[A-Z]'):
+        winner = random.randint(1,range)
+        count = []
+        #제비뽑기 진행
+        while True:
+            num = int(input("원하시는 숫자를 입력해주세요 : "))
+            count.append(num)
+        #당첨
+            if num == winner:
+                print("당첨입니다ㅋ")
+                break
+        #입력했던값이랑 중복되는지 판별
+
+        #범위 판독
+            elif num<1 or num>range:
+                print("범위 내의 숫자를 다시 입력해주세요")
+        #탈락
+            elif num != winner:
+                print("아쉽네요~")
+
+    else:
+        print("숫자로 다시입력해주세요")
