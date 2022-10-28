3e627bc63f479b1c7934f0700849186b43f52b0a
chanuk0 <115782178+chanuk0@users.noreply.github.com>
2022-10-28 15:00:23 +0900

    main.py

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 import telegram
+from telegram import ChatAction
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import MessageHandler, Filters, CommandHandler, Updater, CallbackQueryHandler
-
 my_token = "5336689796:AAGL3VEA1xM9dDodIbDxDNvv6Fv-VirCQoA"
 bot = telegram.Bot(token=my_token)
 id = 5775004281
 bot.sendMessage(chat_id=id, text="나는 대화챗봇입니다.")
-
 updater = Updater(token=my_token, use_context=True)
 dispatcher = updater.dispatcher
 updater.start_polling()
-
-
 def echo(update, context):
     user_text = update.message.text 
     if user_text == "안녕":
@@ -22,7 +19,6 @@ def echo(update, context):
 def ncal(update, context):
     input = context.args[0]
     bot.sendMessage(chat_id=id, text=input)
-
 def build_menu(buttons, n_cols, header_buttons=None, footer_buttons=None):
     menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
     if header_buttons:
@@ -30,7 +26,6 @@ def build_menu(buttons, n_cols, header_buttons=None, footer_buttons=None):
     if footer_buttons:
         menu.append(footer_buttons)
     return menu
-
 def get_school_level(update, context):
     show_list = []
     show_list.append(InlineKeyboardButton("초등학교", callback_data="초등학교")) # add on button
@@ -38,18 +33,17 @@ def get_school_level(update, context):
     show_list.append(InlineKeyboardButton("고등학교", callback_data="고등학교")) # add cancel button
     show_markup = InlineKeyboardMarkup(build_menu(show_list, len(show_list))) # make markup build_menu(리스트, 1줄에 표시할 리스트 수)
     bot.sendMessage(chat_id=id, text="학교 급을 선택해주세요", reply_markup = show_markup) #https://blog.psangwoo.com/coding/2018/08/20/python-telegram-bot-4.html
-
 def callback_get(update, context):
-    pn = update.callback_query.data
+    query = update.callback_query
+    data = query.data
+    context.bot.edit_message_text(
+        text=f"{data}작업을 완료했습니다."
+        , chat_id=query.message.chat_id
+        , message_id=query.message.message_id
+    )
     
 echo_handler = MessageHandler(Filters.text, echo)
 updater.dispatcher.add_handler(CommandHandler('ncal', ncal))
 updater.dispatcher.add_handler(CommandHandler('meal', get_school_level))
 updater.dispatcher.add_handler(CallbackQueryHandler(callback_get))
 dispatcher.add_handler(echo_handler)
-
-
-#ㄲㅂㅇ
-
-
-
