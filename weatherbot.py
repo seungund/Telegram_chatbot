5adbdb3528e2735978240d1b644eb10012819c8b
Lee Seungun <96976715+seungund@users.noreply.github.com>
2022-11-28 23:11:41 +0900

    weather fixed

--- weatherbot.py
+++ weatherbot.py
@@ -61,7 +61,8 @@ time = int('6')
 for ii in range(1,19):
     if wer_li['{}'.format(time)] == "0s" :
         a = wew_li['{}'.format(time)]
-        wer_li['{}'.format(time)] = '{}'.format(a+'s')
+        wer_li['{}'.format(time)] = '{}'.format(a+'n')
+        
     time = time  + 1
     continue   
 
@@ -93,12 +94,14 @@ final = "6시 :{} / {}℃\n7시 :{} / {}℃\n8시 :{} / {}℃\n9시 :{} / {}℃\
 ,wer_li['14'],temp_li['14'],wer_li['15'],temp_li['15'],wer_li['16'],temp_li['16'],wer_li['17'],temp_li['17'],wer_li['18'],temp_li['18'],wer_li['19'],temp_li['19']\
 ,wer_li['20'],temp_li['20'],wer_li['21'],temp_li['21'],wer_li['22'],temp_li['22'],wer_li['23'],temp_li['23'])
 
-final = final.replace('1s','맑음')
-final = final.replace('3s','구름 약간')
-final = final.replace('4s','흐림')
-final = final.replace('1n','비')
-final = final.replace('2n','진눈깨비')
-final = final.replace('3n','눈')
-final = final.replace('4n','소나기')
+final = final.replace('1n','맑음')
+final = final.replace('3n','구름 약간')
+final = final.replace('4n','흐림')
+final = final.replace('1s','비')
+final = final.replace('2s','진눈깨비')
+final = final.replace('3s','눈')
+final = final.replace('4s','소나기')
 final = '<금일({})의 날씨>\n'.format(whole_date) + final
 print(final)
+
+print(wer_li)
